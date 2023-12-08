29ff76d41887385648266c458c8b3628d02e30d2
Sunjay Armstead <connect@sunjayarmstead.local>
2023-12-08 05:43:04 -0500

    Add interior layout

--- web/src/pages/EventInvitePage/EventInvitePage.tsx
+++ web/src/pages/EventInvitePage/EventInvitePage.tsx
@@ -1,20 +1,13 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+
+import InteriorLayout from 'src/layouts/InteriorLayout/InteriorLayout'
 
 const EventInvitePage = () => {
   return (
     <>
       <MetaTags title="EventInvite" description="EventInvite page" />
 
-      <h1>EventInvitePage</h1>
-      <p>
-        Find me in{' '}
-        <code>./web/src/pages/EventInvitePage/EventInvitePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>eventInvite</code>, link to me with `
-        <Link to={routes.eventInvite()}>EventInvite</Link>`
-      </p>
+      <InteriorLayout></InteriorLayout>
     </>
   )
 }
